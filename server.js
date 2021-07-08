c7210f4f45613fcd7ecdc5c07b595e38604521b4
Carlos Martorini <c7.silveira@gmail.com>
2021-07-08 11:39:57 -0300

    initial commit

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const auth = require("json-server-auth");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 
 app.db = router.db;
 
 const rules = auth.rewriter({
-  "/users*": "/600/users$1",
-  "/products*": "/640/products$1"
+  "users*":"/600/users$1",
+  "resumes*": "/664/resumes$1",
+  "messages*": "/messages$1",
+  "companies*":"/600/companies$1"
 });
 
+app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
